21161103f3edae953efdc50e8e11911ae6aa55ea
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-10-16 18:09:51 +0330

    same function for 1D arrary

--- datasets/spiralData.go
+++ datasets/spiralData.go
@@ -1,19 +1,16 @@
 package datasets
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
 
-type coordinate struct {
-	X float64
-	Y float64
-}
 
 //TODO: use sin and cos for points coordinate
 
-func SpiralData(samples, classes int) ([]coordinate, []int) {
-	coor := make([]coordinate, 0, samples)
+func SpiralData(samples, classes int) ([][]float64, []int) {
+	coor := make([][]float64, 0, samples)
 	class := make([]int, 0, samples)
 	var classNum int = 0
 	if samples % classes == 0 {
@@ -23,7 +20,32 @@ func SpiralData(samples, classes int) ([]coordinate, []int) {
 	}
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < samples; i++ {
-		coor = append(coor, coordinate{X: rand.Float64() - rand.Float64(), Y: rand.Float64() - rand.Float64()})
+		coor = append(coor, []float64{rand.NormFloat64(), rand.NormFloat64()})
+	}
+	for c := 0; c < classes; c++ {
+		for j := 0; j < classNum; j++ {
+			class = append(class, c)
+		}
+	}
+	if lenght := len(class) - samples; lenght != 0 {
+		class = class[:samples]
+	}
+	fmt.Println(coor)
+	return coor, class
+}
+
+func SpiralData1(samples, classes int) ([]float64, []int) {
+	coor := []float64{}
+	class := make([]int, 0, samples)
+	var classNum int = 0
+	if samples % classes == 0 {
+		classNum = int(samples / classes)
+	} else {
+		classNum = int(samples / classes) + 1
+	}
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < samples; i++ {
+		coor = append(coor, rand.NormFloat64())
 	}
 	for c := 0; c < classes; c++ {
 		for j := 0; j < classNum; j++ {
