5ef1bcf3d9bd583f69f6e048324d4ef374bf91a1
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-10-21 20:38:47 +0330

    change relu func to relu struct of tensor type

--- activationfunc/ReLU.go
+++ activationfunc/ReLU.go
@@ -1,11 +1,21 @@
 package activationfunc
 
-import "math"
+import (
+	"github.com/Mohammad-Hakemi22/NN/utility"
+	ts "gorgonia.org/tensor"
+)
 
-func ReLU(input []float64) []float64 {
-	output := []float64{}
-	for _, val := range output {
-		output = append(output, math.Max(0, val))
-	}
-	return output
+type ReLU struct {
+	Output ts.Tensor
+}
+
+func NewReLU() *ReLU {
+	return &ReLU{Output: ts.New(ts.Of(ts.Float64))}
+}
+
+func (r *ReLU) Forward(input ts.Tensor) {
+	zeros := ts.New(ts.WithShape(input.Shape()...), ts.Of(ts.Float64))
+	out, err := ts.MaxBetween(zeros, input)
+	utility.ErrorHandler("something went wrong in calculate max:", err)
+	r.Output = out
 }
