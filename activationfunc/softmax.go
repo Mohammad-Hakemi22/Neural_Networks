81581d4504b78faffa915b8cffdad4edd9782d0d
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-10-22 23:41:34 +0330

    update softmax with tensor

--- activationfunc/softmax.go
+++ activationfunc/softmax.go
@@ -1,15 +1,32 @@
 package activationfunc
 
-import "math"
+import (
+	"github.com/Mohammad-Hakemi22/NN/utility"
+	ts "gorgonia.org/tensor"
+)
 
-func SoftMax(input []float64) []float64 {
-	output := []float64{}
-	var sum float64 = 0
-	for _, val := range input {
-		sum += math.Pow(math.E, val)
-	}
-	for _, val := range input {
-		output = append(output, val/sum)
-	}
-	return output
+type SoftMax struct {
+	Output ts.Tensor
 }
+
+func NewSoftMax() *SoftMax {
+	return &SoftMax{Output: ts.New(ts.Of(ts.Float64))}
+}
+
+func (s *SoftMax) Forward(input ts.Tensor, axis int) {
+	out, err := ts.SoftMax(input, axis)
+	utility.ErrorHandler("something went wrong in calculate softmax:", err)
+	s.Output = out
+}
+
+// func SoftMax(input []float64) []float64 {
+// 	output := []float64{}
+// 	var sum float64 = 0
+// 	for _, val := range input {
+// 		sum += math.Pow(math.E, val)
+// 	}
+// 	for _, val := range input {
+// 		output = append(output, val/sum)
+// 	}
+// 	return output
+// }
